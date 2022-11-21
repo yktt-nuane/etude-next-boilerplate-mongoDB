23f61f681693e333c0ddc923448747ce053a1f34
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-11-21 12:25:58 +0900

    add swr

--- src/pages/testpage.tsx
+++ src/pages/testpage.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
 import useSWR from 'swr'
-import styles from '../styles/Home.module.css'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-const Swrpage: NextPage = () => {
-  const { data, error } = useSWR('http://localhost:3000/api/users', fetcher)
+const Test: NextPage = () => {
+  const { data, error } = useSWR('/api/users', fetcher)
 
   if (error) return <div>An error has occurred.</div>
   if (!data) return <div>Loading...</div>
@@ -19,4 +18,4 @@ const Swrpage: NextPage = () => {
   )
 }
 
-export default Swrpage
+export default Test
