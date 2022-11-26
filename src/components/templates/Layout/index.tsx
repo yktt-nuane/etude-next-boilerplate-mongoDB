5600f668d6a78ee9615c852a6b2793624ac8fa5d
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-11-26 09:01:35 +0900

    layout

--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,25 +1,26 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import React, { ReactNode } from 'react'
+import Separator from 'components/atoms/Separator'
+import Box from 'components/layout/Box'
+import Footer from 'components/organisms/Footer'
 
-type Props = {
+type LayoutProps = {
   children?: ReactNode
-  title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children }: LayoutProps) => (
   <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
     <header>
       <nav>
         <Link href='/'>Home</Link> | <Link href='/testpage'>Testpage</Link> |<Link href='/api/users'>Users API</Link>
       </nav>
     </header>
     {children}
+    <Separator />
+    <Box padding={3}>
+      <Footer />
+    </Box>
   </div>
 )
 
